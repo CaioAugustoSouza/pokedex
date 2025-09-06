0644e6268565a7e3c949f6a9457e9d2bdf5da72a
CaioAugustoSouza <caio.mdsistema@gmail.com>
2025-09-05 23:31:58 -0300

    ok

--- App.js
+++ App.js
@@ -1,75 +1,141 @@
+import { SharedObjectType } from "expo";
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Dimensions, ScrollView, TextInput, TouchableOpacity, Image } from "react-native";
 
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 export default function App() {
   const [pokemonList, setPokemonList] = useState([])
-  function obterPokemon(){
-    fetch('https://pokeapi.co/api/v2/pokemon').then(r=>{
-      console.log(r);
+  const [filterList, setFilterList] = useState([])
+  const [image, setImage] = useState()
+  const [tipo, setTipo] = useState()
+  const [nome, setNome] = useState()
+
+  function obterPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0')
+      .then(r => r.json())
+      .then(r => {
+        setPokemonList(r.results.map(response => response))
+      })
+      .catch(error => {
+        console.error('Erro ao obter Pokémon:', error);
+        setPokemonList([])
+      });
+  }
+
+  function buscar() {
+    console.log(busca)
+    console.log(pokemonList)
+    if (busca && busca != '') {
+      setFilterList(
+        pokemonList.filter(
+          item => item.name.toLowerCase().includes(busca.toLowerCase())
+        ))
+      console.log(pokemonList.filter(
+        item => item.name.toLowerCase().includes(busca.toLowerCase())
+      ))
+    }
+  }
+
+  function verDetalhes(url) {
+    fetch(url).then(r => r.json()).then(r => {
+      console.log(r.sprites.other["official-artwork"].front_default)
+      setImage(r.sprites.other["official-artwork"].front_default)
+      console.log(r.types[0].type.name)
+      setTipo(r.types[0].type.name)
+      console.log(r.name)
+      setNome(r.name)
+      fetch(r.types[0].type.url).then(r => r.json()).then(r => {
+        console.log(r.sprites['generation-iii'].colosseum.name_icon)
+        setTipo(r.sprites['generation-iii'].colosseum.name_icon)
+      })
     })
   }
-  const items = [
-    "Banana",
-    "Maçã super gigante que não cabe",
-    "Pera",
-    "Uva",
-    "Laranja",
-    "Abacaxi",
-    "Manga muito grande também",
-    "Melancia",
-    "Morango",
-    "Kiwi",
-    "Cereja",
-    "Goiaba",
-    "Pitaya",
-  ];
+
+  function Limpar() {
+    setFilterList([])
+    setBusca()
+    setImage()
+    setTipo()
+    setNome()
+  }
+
   const [busca, setBusca] = useState()
 
-  useEffect(()=>{
+  useEffect(() => {
     obterPokemon();
   }, [])
 
   return (
     <View style={styles.container}>
       <View style={styles.title}>
-          <Text style={styles.titleText}>
-            Pokedex
-          </Text>
+        <Text style={styles.titleText}>
+          Pokedex
+        </Text>
       </View>
+      <View style={{ margin: 5, backgroundColor: 'white', padding: 6, borderRadius: 4 }}><Text style={{ fontSize: 30 }}>{nome ? nome : ''}</Text></View>
       <View style={styles.display}>
-        <Image
+        {image ?
+          <Image
+            style={styles.image}
+            source={{ uri: image }}
+            resizeMode="contain"
+          />
+          :
+          <></>
+        }
 
-        />
       </View>
+      {tipo ?
+        <Image
+          style={{ width: 100, height: 30, marginBottom: 30, marginTop: 5 }}
+          source={{ uri: tipo }}
+          resizeMode="contain"
+        />
+
+        : <></>
+      }
       <View style={styles.namesBox}>
         <ScrollView showsVerticalScrollIndicator={true}>
-          {items.map((item, index) => (
-            <View key={index} style={styles.item}>
-              <Text style={styles.text}>
-                {item}
-              </Text>
-            </View>
-          ))}
+
+          {
+            filterList.length > 0 ? filterList.map((item, index) => (
+              <View key={index} style={styles.item}>
+                <Text style={styles.text} onPress={() => { verDetalhes(item.url) }}>
+                  {item.name}
+                </Text>
+              </View>
+            ))
+
+              :
+              (pokemonList.length > 0 ? pokemonList.map((item, index) => (
+                <View key={index} style={styles.item}>
+                  <Text style={styles.text} onPress={() => { verDetalhes(item.url) }}>
+                    {item.name}
+                  </Text>
+                </View>
+              )) : <></>
+              )
+
+          }
         </ScrollView>
       </View>
       <View style={styles.inputTextContainer}>
         <TextInput
-          placeholder='digite aqui'
+          placeholder="Buscar pokemon"
           value={busca}
-          onChange={(val) => { setBusca(val) }}
+          onChangeText={(val) => setBusca(val)} // <<< aqui pega só o texto
           style={styles.inputText}
         />
       </View>
       <View style={styles.containerButtons}>
-        <TouchableOpacity style={styles.btn1} >
+        <TouchableOpacity style={styles.btn1} onPress={buscar}>
           <Text style={styles.btnText}>
             Buscar
           </Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.btn2}>
-          <Text style={styles.btnText2}>
+          <Text style={styles.btnText2} onPress={Limpar}>
             Limpar
           </Text>
         </TouchableOpacity>
@@ -79,6 +145,11 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  image: {
+    width: width * 0.8,
+    height: height * 0.8,
+    marginTop: 0
+  },
   titleText: {
     textAlign: 'center'
   },
@@ -92,11 +163,13 @@ const styles = StyleSheet.create({
     borderRadius: 10
   },
   display: {
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
     width: 8 * width / 9,
     height: 400,
-    marginBottom: 30,
-    borderRadius: 40
+    borderRadius: 40,
+    justifyContent: 'center',
+    display: 'flex',
+    alignItems: 'center'
   },
   container: {
     display: 'flex',
@@ -126,12 +199,11 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   },
   inputTextContainer: {
-    backgroundColor: 'blue',
     marginTop: 30,
     width: width / 2,
   },
   inputText: {
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     borderRadius: 10
   },
   containerButtons: {
@@ -156,11 +228,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     justifyContent: 'center'
   },
-  btnText:{
+  btnText: {
     textAlign: 'center',
     color: 'white'
   },
-  btnText2:{
+  btnText2: {
     textAlign: 'center',
     color: 'black'
   }
